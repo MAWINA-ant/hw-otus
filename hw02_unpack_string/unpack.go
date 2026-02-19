364b793607b493d4f4e39bbeec3185f0b055a1ec
Ruslan Muzhichkov <chaika1201@gmail.com>
2026-02-20 01:14:30 +0300

    first attempt

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,23 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
+func Unpack(str string) (string, error) {
+	strRunes := []rune(str)
+	var currentRune rune
+	for _, c := range strRunes {
+		if currentRune == 0 {
+			if unicode.IsDigit(c) {
+				return "", ErrInvalidString
+			} else {
+				currentRune = c
+			}
+		}
+
+	}
 	return "", nil
 }
