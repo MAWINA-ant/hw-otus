f2d55ea6e8b792a9f5e4e20f167df276b25232e4
Ruslan Muzhichkov <chaika1201@gmail.com>
2026-03-03 00:51:55 +0300

    update cache for passing tests

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -8,6 +8,11 @@ type Cache interface {
 	Clear()
 }
 
+type pair struct {
+	first  Key
+	second interface{}
+}
+
 type lruCache struct {
 	capacity int
 	queue    List
@@ -17,21 +22,28 @@ type lruCache struct {
 func (c *lruCache) Set(key Key, value interface{}) bool {
 	if v, ok := c.items[key]; ok {
 		c.queue.MoveToFront(v)
+		v.Value = pair{key, value}
 		return true
 	}
 	if c.queue.Len() == c.capacity {
-
+		delete(c.items, c.queue.Back().Value.(pair).first)
+		c.queue.Remove(c.queue.Back())
 	}
-	c.items[key] = c.queue.PushFront(value)
+	c.items[key] = c.queue.PushFront(pair{key, value})
 	return false
 }
 
 func (c *lruCache) Get(key Key) (interface{}, bool) {
+	if v, ok := c.items[key]; ok {
+		c.queue.MoveToFront(v)
+		return c.queue.Front().Value.(pair).second, true
+	}
 	return nil, false
 }
 
 func (c *lruCache) Clear() {
-
+	c.queue = NewList()
+	c.items = make(map[Key]*ListItem, c.capacity)
 }
 
 func NewCache(capacity int) Cache {
