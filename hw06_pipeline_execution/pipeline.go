56ba5cfe59fd9b6a0760c21652443777b880b173
Ruslan Muzhichkov <chaika1201@gmail.com>
2026-04-01 23:16:27 +0300

    Did hometask number 6

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,25 @@ type (
 
 type Stage func(in In) (out Out)
 
+func runStage(in In, done In, stage Stage) Out {
+	out := make(Bi)
+	go func() {
+		defer close(out)
+		for i := range stage(in) {
+			select {
+			case <-done:
+				return
+			case out <- i:
+			}
+		}
+	}()
+	return out
+}
+
 func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+	currentIn := in
+	for _, stage := range stages {
+		currentIn = runStage(currentIn, done, stage)
+	}
+	return currentIn
 }
