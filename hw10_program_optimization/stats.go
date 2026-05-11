dd2b4bfc04db6defa7c53889a12561ff07e4b479
Ruslan Muzhichkov <ruslan.muzhichkov@c3dlabs.com>
2026-05-11 23:52:31 +0300

    Change regexp

--- hw10_program_optimization/stats.go
+++ hw10_program_optimization/stats.go
@@ -12,7 +12,7 @@ type DomainStat map[string]int
 func GetDomainStat(r io.Reader, domain string) (DomainStat, error) {
 	result := make(DomainStat)
 	reader := bufio.NewReader(r)
-	emailRegex := regexp.MustCompile(`[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}`)
+	emailRegex := regexp.MustCompile(`@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}`)
 	for {
 		line, err := reader.ReadString('\n')
 		if err != nil && err != io.EOF {
