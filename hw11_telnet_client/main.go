dcc4bd1b9ec762448eaf427272ee278d98aac179
Ruslan Muzhichkov <ruslan.muzhichkov@c3dlabs.com>
2026-05-15 14:45:48 +0300

    some changes in telnet

--- hw11_telnet_client/main.go
+++ hw11_telnet_client/main.go
@@ -1,6 +1,34 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+	"time"
+)
+
+var ErrorFewArguments = errors.New("not enougth arguments")
+var ErrorBadArguments = errors.New("incorrect arguments")
+
 func main() {
+	args := os.Args
+	if len(args) < 3 {
+		fmt.Println(ErrorFewArguments)
+		os.Exit(1)
+	}
+	t, _ := time.ParseDuration("10s")
+	if strings.Contains(args[1], "--timeout=") {
+		if len(args) < 4 {
+			fmt.Println(ErrorBadArguments)
+			os.Exit(1)
+		}
+		t, _ = time.ParseDuration(strings.ReplaceAll(args[1], "--timeout=", ""))
+	}
+	host := strings.Join(args[2:4], ":")
+	telnetClient := NewTelnetClient(host, t, os.Stdout, os.Stdin)
+	telnetClient.Connect()
+
 	// Place your code here,
 	// P.S. Do not rush to throw context down, think think if it is useful with blocking operation?
 }
--- hw11_telnet_client/telnet.go
+++ hw11_telnet_client/telnet.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"io"
+	"net"
 	"time"
 )
 
@@ -13,9 +14,48 @@ type TelnetClient interface {
 }
 
 func NewTelnetClient(address string, timeout time.Duration, in io.ReadCloser, out io.Writer) TelnetClient {
-	// Place your code here.
+	telnetStruct := TelnetStruct{
+		address: address,
+		timeout: timeout,
+		in:      in,
+		out:     out,
+	}
+	return &telnetStruct
+}
+
+type TelnetStruct struct {
+	address string
+	timeout time.Duration
+	in      io.ReadCloser
+	out     io.Writer
+	con     net.Conn
+}
+
+func (t *TelnetStruct) Connect() error {
+	con, err := net.DialTimeout("tcp", t.address, t.timeout)
+	if err != nil {
+		return err
+	}
+	t.con = con
 	return nil
 }
 
-// Place your code here.
-// P.S. Author's solution takes no more than 50 lines.
+func (t *TelnetStruct) Close() error {
+	return t.Close()
+}
+
+func (t *TelnetStruct) Send() error {
+	_, err := io.Copy(t.con, t.in)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (t *TelnetStruct) Receive() error {
+	_, err := io.Copy(t.out, t.con)
+	if err != nil {
+		return err
+	}
+	return nil
+}
